5343cee8024dafd3c159e605fb21d112da396807
TODO <todo-actions[bot]@users.noreply.github.com>
2020-01-22 11:26:26 +0000

    Collect TODO comments

--- tests/test_doi.py
+++ tests/test_doi.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-# TODO: add actual tests, not just a mock test
+# TODO [$5e283162288294000703852f]: add actual tests, not just a mock test
 
 def test_mock():
     return True
