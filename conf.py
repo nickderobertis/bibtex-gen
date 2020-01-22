890f36b0fb43c8bace0e371916d2b68ba2ed14d2
Nick DeRobertis <whoopnip@gmail.com>
2020-01-22 09:11:58 -0500

    Add mendeley2 and pyexlatex to required packages

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ REPO_MAINTAINERS = [
 ]
 
 # Package version in the format (major, minor, release)
-PACKAGE_VERSION_TUPLE = (0, 1, 0)
+PACKAGE_VERSION_TUPLE = (0, 1, 1)
 
 # Short description of the package
 PACKAGE_SHORT_DESCRIPTION = "Generate BibTeX reference objects from DOIs and strings"
@@ -59,6 +59,8 @@ PACKAGE_INSTALL_REQUIRES = [
     # e.g.
     # 'package',
     # 'otherpackage>=1,<2'
+    'mendeley2',
+    'pyexlatex'
 ]
 
 # Add any third party packages you use in requirements for optional features of your package here
