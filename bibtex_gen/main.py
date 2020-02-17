2987dbf8d800a1dce7186478fb4e20beb58c9210
Nick DeRobertis <whoopnip@gmail.com>
2020-02-17 17:17:49 -0500

    Update docs

--- bibtex_gen/main.py
+++ bibtex_gen/main.py
@@ -13,12 +13,10 @@ class BibTexGenerator:
     :Examples:
 
         >>> import bibtex_gen
-        >>>
         >>> # Note: these are fake credentials. You need to sign up for a Mendeley account, go to Mendeley Developers,
         >>> # and create an "app" which will give you this info.
         >>> mendeley_client_id: str = '9871'
         >>> mendeley_client_secret: str = 'sdfa4dfDSSDFasda'
-        >>>
         >>> btg = bibtex_gen.BibTexGenerator(mendeley_client_id, mendeley_client_secret)
         >>> bibtex_obj = btg.generate('10.1111/j.1540-6261.2011.01679.x', 'da-engelberg-gao-2011')
     """
@@ -67,13 +65,11 @@ def item_accessor_doi_dict_to_bib_tex(item_doi_dict: Dict[str, str], mendeley_cl
         >>> # and create an "app" which will give you this info.
         >>> mendeley_client_id: str = '9871'
         >>> mendeley_client_secret: str = 'sdfa4dfDSSDFasda'
-        >>>
         >>> # Keys are name by which reference will be accessed, values are DOIs
         >>> item_doi_dict = {
         >>> 'da-engelberg-gao-2011': '10.1111/j.1540-6261.2011.01679.x',
         >>> 'barber-odean-2008': '10.1093/rfs/hhm079',
         >>> }
-        >>>
         >>> # These objects contain all the article data and can be used directly with pyexlatex
         >>> bibtex_objs = bibtex_gen.item_accessor_doi_dict_to_bib_tex(item_doi_dict, mendeley_client_id, mendeley_client_secret)
 
